5d16f63b54fd7b01d7fc6a90bf38266d340c06a8
unknown <andresirias02@gmail.com>
2022-03-26 13:08:12 -0600

    Arreglar agregar autores en localhost

---
+++ Atestados.Utilitarios/Variables/Variables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Atestados.Utilitarios.Variables
+{
+    public static class Variables
+    {
+        public static string BaseUrl { get; set; }
+
+        static Variables()
+        {
+            if (HttpContext.Current.Request.Url.Host.Equals("localhost"))
+            {
+                BaseUrl = "/Atestados";
+            }
+        }
+
+
+    }
+}
