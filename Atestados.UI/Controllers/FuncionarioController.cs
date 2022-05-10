81ebd426c415c47b546ff551af47cbb0c84c981f
calo <carlos.roberto.kruse@gmail.com>
2022-05-10 14:27:08 -0600

    SIRA-13 Se puede ver evaluación ponderada en Libro/Ver

--- Atestados.UI/Controllers/FuncionarioController.cs
+++ Atestados.UI/Controllers/FuncionarioController.cs
@@ -125,5 +125,27 @@ namespace Atestados.UI.Controllers
 
         }
 
+        [HttpPost]
+        public float notaAtestado(AtestadoDTO atestado)
+        {
+            var id = atestado.AtestadoID;
+            var evaluaciones = infoAtestado.ObtenerEvaluacionesAtestado(id);
+            if (evaluaciones.Count == 0)
+            {
+                return 0;
+            }
+
+            var porcentaje = .0;
+
+            foreach (EvaluaciónXAtestado evaluacion in evaluaciones)  {
+                porcentaje += evaluacion.PorcentajeObtenido;
+            }
+
+            var r = porcentaje / evaluaciones.Count;
+
+            return (float) r;
+
+        }
+
     }
 }
