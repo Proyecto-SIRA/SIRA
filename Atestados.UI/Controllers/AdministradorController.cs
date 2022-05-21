cd18298db128d3204c117d497e413a5b9d61bb82
unknown <andresirias02@gmail.com>
2022-05-20 23:55:07 -0600

    Crear DTO para información del progreso de los puntos

--- Atestados.UI/Controllers/AdministradorController.cs
+++ Atestados.UI/Controllers/AdministradorController.cs
@@ -52,6 +52,25 @@ namespace Atestados.UI.Controllers
         [HttpGet]
         public ActionResult Ver(int id)
         {
+            List<PuntosXRubroDTO> puntosXRubroDTOs = new List<PuntosXRubroDTO>();
+
+            PuntosXRubroDTO puntosXRubroDTO = new PuntosXRubroDTO();
+            puntosXRubroDTO.Rubro = "Total";
+            puntosXRubroDTO.PuntosPasoActual = 5;
+            puntosXRubroDTO.PuntosMaximosPasoActual = 10;
+            puntosXRubroDTO.PuntosAcumulados = 25;
+
+            PuntosXRubroDTO puntosXRubroDTO2 = new PuntosXRubroDTO();
+            puntosXRubroDTO2.Rubro = "Total 2";
+            puntosXRubroDTO2.PuntosPasoActual = 6;
+            puntosXRubroDTO2.PuntosMaximosPasoActual = 16;
+            puntosXRubroDTO2.PuntosAcumulados = 23;
+
+            puntosXRubroDTOs.Add(puntosXRubroDTO);
+            puntosXRubroDTOs.Add(puntosXRubroDTO2);
+
+            ViewBag.PuntosXRubroDTOs = puntosXRubroDTOs;
+
             return View(info.CargarPersona(id));
         }
 
